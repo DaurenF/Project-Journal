bd5f22a4b5771b51911eae220151ab508832d50e
Dauren <98801908+DaurenF@users.noreply.github.com>
2023-01-07 13:34:12 +0600

    Add HomeController Test and lombok to model

--- src/main/java/com/example/demo/models/User.java
+++ src/main/java/com/example/demo/models/User.java
@@ -5,14 +5,18 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 import jakarta.validation.constraints.Email;
-import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotEmpty;
-import jakarta.validation.constraints.NotNull;
+import lombok.*;
 
 import java.util.Date;
 
 @Entity
 @Table(name = "users")
+@ToString
+@Getter
+@Setter
+@NoArgsConstructor
+@EqualsAndHashCode
 public class User {
     @Id
     @Column(name = "id")
@@ -40,17 +44,6 @@ public class User {
     @Column(name = "exp_date")
     private Date exp_date;
 
-    public Date getExp_date() {
-        return exp_date;
-    }
-
-    public void setExp_date(Date exp_date) {
-        this.exp_date = exp_date;
-    }
-
-    public User() {
-    }
-
     public User(String name, String surname, Date birth, String gender, String country, String email, String password) {
         this.name = name;
         this.surname = surname;
@@ -61,67 +54,5 @@ public class User {
         this.password = password;
     }
 
-    public long getId() {
-        return id;
-    }
 
-    public void setId(long id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getSurname() {
-        return surname;
-    }
-
-    public void setSurname(String surname) {
-        this.surname = surname;
-    }
-
-    public Date getBirth() {
-        return birth;
-    }
-
-    public void setBirth(Date birth) {
-        this.birth = birth;
-    }
-
-    public String getGender() {
-        return gender;
-    }
-
-    public void setGender(String gender) {
-        this.gender = gender;
-    }
-
-    public String getCountry() {
-        return country;
-    }
-
-    public void setCountry(String country) {
-        this.country = country;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
-    }
 }
